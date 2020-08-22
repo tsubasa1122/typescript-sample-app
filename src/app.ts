ee16913ace472762abf1e2b541f7a28b5e74a5f3
tsubasa1122 <nakanot@open8.com>
2020-08-22 16:20:21 +0900

    Show Google Maps

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,32 @@ import axios from 'axios';
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
+// const GOOGLE_API_KEY = YOUR API KEY;
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[],
+  status: 'OK' | 'ZERO_RESULTS';
+};
+
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredValue = addressInput.value;
 
-
+  axios.get<GoogleGeocodingResponse>(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredValue)}&key=${GOOGLE_API_KEY}
+  `).then(response => {
+    if (response.data.status !== 'OK') {
+      throw new Error('座標を取得できませんでした。');
+    }
+    const coordinates = response.data.results[0].geometry.location;
+    const map = new google.maps.Map(document.getElementById('map')!, {
+      center: coordinates,
+      zoom: 16
+    });
+    new google.maps.Marker({ position: coordinates, map: map });
+  }).catch(err => {
+    alert(err.message);
+    console.log(err);
+  });
 };
 
 form.addEventListener('submit', searchAddressHandler);
